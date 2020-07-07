5c78a038ab45afe63f4e805abd9f7ab1d023ed6e
Enis Simsar <enisimsar@gmail.com>
2020-07-07 23:47:11 +0300

    change api return type and add an example notebook

--- rest.py
+++ rest.py
@@ -1,15 +1,24 @@
 import io
 import cv2
 import base64
+import random
+import string
 import numpy as np
 
 from tools import get_res
 from base64 import decodestring
 from fastapi import FastAPI, File
+from fastapi.staticfiles import StaticFiles
 from starlette.requests import Request
 from starlette.responses import StreamingResponse
 
+def random_string(string_length=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(string_length))
+
 app = FastAPI()
+
+app.mount("/results", StaticFiles(directory="results"), name="results")
 
 @app.post("/predict")
 def predict(request: Request, 
@@ -19,4 +28,9 @@ def predict(request: Request,
     _, res_img = get_res(img)
     res_img = cv2.cvtColor(res_img, cv2.COLOR_RGB2BGR)
 
-    return base64.b64encode(res_img.tobytes())
+    img_name = f'results/{random_string()}.png'
+    cv2.imwrite(img_name, res_img)
+
+    return {
+            "file_name": img_name
+    } #base64.b64encode(res_img.tobytes())
